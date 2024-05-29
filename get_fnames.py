b51d4f65548db833a1cb18a8933545b570ed1c1b
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-05-29 12:51:13 +0800

    added compare view

--- get_fnames.py
+++ get_fnames.py
@@ -2,7 +2,7 @@ import os
 import json
 
 fnames = os.listdir("public/")
-fnames = [f for f in fnames if f.endswith(".json") and f not in ["manifest.json", "fnames.json"]]
+fnames = sorted([f for f in fnames if f.endswith(".json") and f not in ["manifest.json", "fnames.json"]])
 
 with open("public/fnames.json", "w") as f:
     json.dump(fnames, f)
